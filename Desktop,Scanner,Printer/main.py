e1c5291141631266ba1e26d6befdabe359d5426e
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-01-17 13:36:03 +0600

    3 class added

--- Desktop,Scanner,Printer/main.py
+++ Desktop,Scanner,Printer/main.py
@@ -1,4 +1,36 @@
-import dictRysan as raf
+class Desktop:
+    def playing_game(self):
+        return "Playing game is an objective."
 
-raf.show_def()
+    def graphics_design(self):
+        return "Graphics Designing is an objective."
 
+    def deployment(self):
+        return "Deploying is an objective."
+
+    def data_sotre(self):
+        return "Data Storing is an objective."
+
+class Scanner:
+    def scannind_docs(self):
+        return "Scanning Documents is an objective."
+
+    def scanning_img(self):
+        return "Scanning Images is an objective."
+
+
+
+class Printer:
+
+    def printing_docs(self):
+        return "Printing Documents is an objective."
+
+    def Printing_img(self):
+        return "Printing Images is an objective."
+
+
+desktop1=Desktop()
+desktop2=Desktop()
+
+print(desktop1.deployment())
+print(desktop2)
