4a55badf9d2b58f2c26023060be245864cf4f5ae
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-01-17 14:51:10 +0600

    Update 8

---
+++ Creating_CLASS_and_OBJECT/main.py
@@ -0,0 +1,39 @@
+# this is our main function
+
+# first you have to creat a class . Lets make 3 classes accordintg to our example
+
+# We have to make a Desktop class where all the objective will be remain inside
+
+class Desktop:
+
+    def playing_game(self):
+        return "Playing game is an objective."
+
+    def graphics_design(self):
+        return "Graphics Designing is an objective."
+
+    def deployment(self):
+        return "Deploying is an objective."
+
+    def data_sotre(self):
+        return "Data Storing is an objective."
+
+
+# We have to make a Scanner class where all the objective will be remain inside
+class Scanner:
+
+    def scannind_docs(self):
+        return "Scanning Documents is an objective."
+
+    def scanning_img(self):
+        return "Scanning Images is an objective."
+
+
+# We have to make a Printer class where all the objective will be remain inside
+class Printer:
+
+    def printing_docs(self):
+        return "Printing Documents is an objective."
+
+    def printing_img(self):
+        return "Printing Images is an objective."
