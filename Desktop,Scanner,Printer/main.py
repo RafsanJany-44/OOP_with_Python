63de5f2fef30f94726889daf7d6f7951cdcb04fc
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-01-17 14:08:45 +0600

    Update 5

--- Desktop,Scanner,Printer/main.py
+++ Desktop,Scanner,Printer/main.py
@@ -35,10 +35,7 @@ class Printer:
 
  supose you need 2 desktop, 1 scanners, 1 printers for different perposes....
 
- desktop1-> deploying,
- desktop2-> Graphics Design,
-   
- (but we know we can do also other three objectives with each desktop)
+    (we know we can do also other three objectives with each desktop)
  
  
  what will you do? you have to buy whose machines and your frnd has the 
@@ -54,3 +51,8 @@ desktop2=Desktop()    #Desktop 2 has been created according to its Bluprint
 print("Desktop1 : ",desktop1.deployment()) #Desktop 1 is doing his job
 
 print("Desktop2 : ",desktop2.graphics_design()) #Desktop 2 is doing his job
+
+#lets do somting with else with this two desktops
+
+print("Desktop1: ",desktop1.data_sotre())   # Desktop1 is doing another work
+print("Desktop2: ",desktop2.playing_game()) # Desktop1 is doing another work
