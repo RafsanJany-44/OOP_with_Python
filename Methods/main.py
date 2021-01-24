1116bce0b4acb9d63c08c1d5a3877fc6e1fb79b6
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-01-24 14:46:07 +0600

    Update 15

---
+++ Methods/main.py
@@ -0,0 +1,41 @@
+"""lets learn Methods"""
+"""
+In easy word, every function inside a class is called method. Like our Desktop class has
+4 methods. 
+"""
+
+#example
+
+"""
+There is 3 kinds of methods.:
+    1. Instance Method.
+    2. Static Method.
+    3. Class Method.
+In this section we will discuss about  Static Method and Class method. Instance will be discussed
+latter.
+"""
+
+'''
+Static Method: By the name, you can understand that static method is static.
+You can not manipulate the function. You just can use it through your object.
+
+In easy word: It has no parameter.
+N.B: Here self is a default parameter. 
+
+In our example, all methods was static.
+'''
+
+#example
+
+'''
+We can only call them and the returns what ever the
+made for.
+
+'''
+
+
+"""
+Class Method: This kind of methods can be manipulate byt the passing values.
+In easy Word: Has Different parameters.
+"""
+
